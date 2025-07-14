b8acd52619d2163f508e2388bee451b66fb905c8
sugtao4423 <sugtao4423@gmail.com>
2025-07-14 17:01:59 +0900

    renew options page

--- src/options.js
+++ src/options.js
@@ -1,11 +1,30 @@
-;(async () => {
-  const options = await chrome.storage.sync.get(['prefix'])
-  const prefix = options.prefix ?? ''
+const [prefixInput, saveButton, message] = document.querySelectorAll(
+  '#prefix, #save, #message'
+)
 
-  const result = prompt('Set Prefix', prefix)
-  if (result !== null) {
-    await chrome.storage.sync.set({ prefix: result })
-    alert(`Prefix Saved!\n'${result}'`)
+const showMessage = (text) => {
+  message.innerText = text
+  setTimeout(() => {
+    message.innerText = ''
+  }, 3000)
+}
+
+saveButton.addEventListener('click', async () => {
+  const newPrefix = prefixInput.value
+  await chrome.storage.sync.set({ prefix: newPrefix })
+  showMessage(`Prefix Saved!\n'${newPrefix}'`)
+})
+
+const main = async () => {
+  try {
+    const result = await chrome.storage.sync.get(['prefix'])
+    prefixInput.value = result.prefix ?? ''
+  } catch (error) {
+    saveButton.disabled = true
+    prefixInput.disabled = true
+
+    showMessage('Error retrieving options. Please try again later.')
+    console.error('Error retrieving options:', error)
   }
-  window.close()
-})()
+}
+main()
